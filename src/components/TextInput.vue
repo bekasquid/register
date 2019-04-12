fad23919da55254527c2c0aead3b326fa79317d1
davids <dheo0@naver.com>
2019-04-13 00:26:51 +0900

    test

---
+++ src/components/TextInput.vue
@@ -0,0 +1,32 @@
+<template>
+    <span class="ps_box int_id" :class="{ focus }">
+        <input type="text" class="int"
+            @focus="inputEvent" 
+            @focusout="inputEvent"
+            @keyup="inputEvent">
+    </span>
+    
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            focus: false
+        }
+    },
+    methods: {
+        inputEvent(e) {
+            console.log(e);
+            this.$emit('inputEvent', e);
+            if ((e.type == 'focus' || e.type == 'keyup') ) {
+                // activate the effect
+                this.focus = true;
+            }
+            else {
+                this.focus = false;
+            }
+        }
+    }
+}
+</script>
