badc7b0da7c2123e3054d8c081cda5bbdd38de23
davids <dheo0@naver.com>
2019-04-14 00:10:20 +0900

    build for test

--- src/components/BasicButton.vue
+++ src/components/BasicButton.vue
@@ -9,10 +9,3 @@ export default {
     props: ['btnText']
 }
 </script>
-<style lang="scss">
-    @import '../assets/css/common.scss';
-    .btn_primary {
-        color: #fff;
-        background-color: $bg_primary;
-    }
-</style>
